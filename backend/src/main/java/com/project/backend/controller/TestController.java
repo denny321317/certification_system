ccd32e9411b724cbaa93d3826c5b5fddcab37884
tp12356twNccu <146693497+tp12356twNccu@users.noreply.github.com>
2025-04-28 00:31:44 +0800

    had some progress, seperated backend from frontend.

--- backend/src/main/java/com/project/backend/controller/TestController.java
+++ backend/src/main/java/com/project/backend/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.project.backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class TestController {
 
     // 測試端點，當你訪問 http://localhost:8080/api/test 時，會回傳 "Backend is running!"
---
+++ backend/src/main/java/com/project/backend/service/UserManagementService.java
@@ -0,0 +1,11 @@
+package com.project.backend.service;
+
+import org.springframework.stereotype.Service;
+import com.project.backend.model.User;
+import com.project.backend.repository.UserRepository;
+
+
+@Service
+public class UserManagementService {
+    
+}
