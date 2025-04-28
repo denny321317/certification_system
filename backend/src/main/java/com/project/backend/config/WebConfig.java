3bc7130ecca9113d6e225f44701dd84cb1dd38d4
tp12356twNccu <146693497+tp12356twNccu@users.noreply.github.com>
2025-04-28 20:55:26 +0800

    made two models in the DB User and Role

--- backend/src/main/java/com/project/backend/config/WebConfig.java
+++ backend/src/main/java/com/project/backend/config/WebConfig.java
@@ -6,21 +6,15 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer(){
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry){
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // React Dev Server
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true)
-                        .maxAge(3600);
-            }
-        };
+public class WebConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // React Dev Server
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
     
 }
--- backend/src/main/java/com/project/backend/repository/UserRepository.java
+++ backend/src/main/java/com/project/backend/repository/UserRepository.java
@@ -3,8 +3,12 @@ package com.project.backend.repository;
 import com.project.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.project.backend.model.Role;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByRole(Role role);
     
 }
