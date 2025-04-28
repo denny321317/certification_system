39f48414c12367c5aa65b1bf2d974a4d65b457ad
tp12356twNccu <146693497+tp12356twNccu@users.noreply.github.com>
2025-04-28 21:26:51 +0800

    not finished, but things in service class should be good, although not tested yet. Controller class is not finished yet, problem with UserRequest class.

--- backend/src/main/java/com/project/backend/controller/UserManagementController.java
+++ backend/src/main/java/com/project/backend/controller/UserManagementController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,18 @@ public class UserManagementController {
     @Autowired
     private UserManagementService userManagementService;
 
+    // TODO: finish the controller mappings
+
     @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = userManagementService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/role/{roleName}")
+    public ResponseEntity<List<User>> getUsersByRole(@PathVariable String roleName){
+        List<User> users = userManagementService.getUsersByRole(roleName);
+        return ResponseEntity.ok(users);
+    }
 
 }
