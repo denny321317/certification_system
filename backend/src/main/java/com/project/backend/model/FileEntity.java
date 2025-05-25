7a2f70a7d09ade7e154de6d81fc8990e621e794b
gtc0823 <gtchen0823@gmail.com>
2025-05-25 20:43:39 +0800

    update backend entity

--- backend/src/main/java/com/project/backend/model/FileEntity.java
+++ backend/src/main/java/com/project/backend/model/FileEntity.java
@@ -2,14 +2,8 @@ package com.project.backend.model;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Id;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
-import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Data
 @AllArgsConstructor
@@ -31,5 +25,9 @@ public class FileEntity {
     private String status;
     private String category;
     private String description;
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+
 
 }
